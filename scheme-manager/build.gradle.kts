e194dcb9b0f4f778128378af593accc1af9169de
Aleksey Ivanov <ivanov.aleks2000-ru@yandex.ru>
2024-09-13 17:30:55 +0300

    - clean scheme-manager subproject build.gradle.kts file

--- scheme-manager/build.gradle.kts
+++ scheme-manager/build.gradle.kts
@@ -6,44 +6,12 @@ plugins {
 }
 
 group = "ru.nti.dpts"
-version = "0.10.0"
-description = "Scheme Manager Back"
+version = "0.0.1"
+description = "Json to CIM converter"
 
 dependencies {
-    implementation(Libs.Kotlin.arrow)
-    implementation(Libs.Dtps.java_dev_kit)
-
-    implementation(Libs.Kotlin.reflect)
-    implementation(Libs.Kotlin.stdlib)
-    implementation(Libs.Jackson.kotlin)
-    implementation(Libs.Jackson.databind)
-
-//    implementation(Libs.Spring.kafka)
     implementation(Libs.Spring.starter_web)
-//    implementation(Libs.Spring.starter_webflux)
-//    implementation(Libs.Spring.starter_websocket)
-//    implementation(Libs.Spring.starter_security)
-//    implementation(Libs.Spring.starter_security_oauth2_client)
-//    implementation(Libs.Spring.starter_security_oauth2_resource_server)
-//    implementation(Libs.Spring.starter_data_mongo)
-//    implementation(Libs.Spring.security_messaging)
-
-    implementation(Libs.Actuator.spring_starter_actuator)
-//    implementation(Libs.Actuator.prometheus)
     implementation(project(":cim-converter"))
-
-    implementation(Libs.Javax.validation)
-    implementation(Libs.Javax.activation)
-
-    implementation(Libs.Jaxb.api)
-    implementation(Libs.Jaxb.runtime)
-
-    implementation(Libs.Caffeine.caffeine)
-
-//    testImplementation(Libs.Test.spring_starter_test)
-//    testImplementation(Libs.Test.flapdoodle_mongo)
-//    testImplementation(Libs.Test.flapdoodle_spring)
-//    testImplementation(Libs.Test.assertj_arrow_core)
 }
 
 springBoot {
